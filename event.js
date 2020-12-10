f0b79c62558d7dafe17599abfca4e28b1dd93bb2
Birgül <44294078+birgullayaz@users.noreply.github.com>
2020-12-10 16:08:06 +0300

    Add files via upload
    keybord events

--- event.js
+++ event.js
@@ -9,3 +9,33 @@ filterInput.addEventListener("focus",function(e){
 e.target.placeholder = "Yeni içerik";
 console.log(e.target.placeholder);
 });
+
+
+//submit ile sayfa yenilenmesini kapatma
+const todoForm = document.getElementById("todo-form");
+todoForm.addEventListener("submit",submitForm);
+function submitForm(e){
+	console.log("Submit Event");
+	e.preventDefault();  //sayfa yenilenmiyor artık
+}
+
+
+//Klavye Eventleri
+
+//Keypress eventleri
+
+//document.addEventListener("keypress",run);
+
+//function run(e){
+	//console.log(e.which);
+	//console.log(e.key);
+//}
+
+
+//Keyup eventleri
+todoInput.addEventListener("keyup",changeText);
+
+function run(e){
+	console.log(e.which);
+	console.log(e.key);
+}
